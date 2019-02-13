774a90f3b36fb550fec645f196863f96b7477940
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-12 22:31:56 -0700

    finished incrementIfOdd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,11 @@ import { connect } from "react-redux";
 import { increment, decrement } from "../actions";
 
 class Counter extends Component {
-  //   incrementIfOdd = () => {
-  //     // Stretch Problem: Implement an increment function that
-  //     // only increments if the counter value is odd
-  //   };
+  incrementIfOdd = () => {
+    // Stretch Problem: Implement an increment function that
+    // only increments if the counter value is odd
+    this.props.count % 2 !== 0 ? this.props.increment(this.props.count) : null;
+  };
 
   //   incrementAsync = () => {
   //     // Stretch Problem: Implement an increment function that
@@ -26,14 +27,8 @@ class Counter extends Component {
         <button onClick={count => this.props.decrement(this.props.count)}>
           -
         </button>
-        {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-        {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>
+        {/* <button onClick={this.incrementAsync}>Increment async</button> */}
       </p>
     );
   }
